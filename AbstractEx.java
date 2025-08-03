83c588edf1558ffda5593a320c880de9ee3f2ef6
Sadabrata Konar <b0278780@airtel.com>
2025-08-03 23:05:37 +0530

    Abstract class

---
+++ AbstractEx.java
@@ -0,0 +1,33 @@
+abstract class Car {
+    public abstract void drive();
+
+    public abstract void fly();
+
+    public void playMusic() {
+        System.out.println("play music");
+    }
+}
+
+abstract class WagnoR extends Car {
+    public void drive() {
+        System.out.println("Driving...");
+    }
+}
+
+class UpdateWagnoR extends WagnoR // concrete class
+{
+    public void fly() {
+        System.out.println("flying...");
+    }
+}
+
+public class AbstractEx {
+    public static void main(String[] args) {
+
+        // Car obj= new Car(); // can not create object of abstract classes
+        // Car obj= new WagnoR();
+        Car obj = new UpdateWagnoR();
+        obj.drive();
+        obj.playMusic();
+    }
+}
