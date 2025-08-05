d82f24f01d7505a9b7c0f2e2d145255d4f7ebc18
Sadabrata Konar <b0278780@airtel.com>
2025-08-06 02:02:20 +0530

    TryCatchThrow

---
+++ TryCatchThrow.java
@@ -0,0 +1,21 @@
+public class TryCatchThrow {
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 0;
+
+        try {
+            j = 18 / i;
+            if (j == 0) {
+                throw new ArithmeticException("I don't want to do print zero");
+            }
+        } catch (ArithmeticException e) {
+            j = 18 / i;
+            System.out.println("that is default output " + e);
+        } catch (Exception e) {
+            System.out.println("Something went wrong. " + e);
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+}
