92c753d3be357cdb652c58351355ad443a3877cc
Sadabrata Konar <b0278780@airtel.com>
2025-08-08 01:11:50 +0530

    race condition and thread states

--- RaceCondition.java
+++ RaceCondition.java
@@ -44,3 +44,14 @@ public class RaceCondition {
         System.out.println(c.count);
     }
 }
+
+/**
+ * - New State
+ * - Runnable State -> start() method
+ * - Running State -> a thead is running with run() method
+ * - Waiting State -> sleep(), wait() method
+ * - Dead State
+ * 
+ * Through notify(), you will go to waiting state to runnable state.
+ * From Running, Runnable state to dead state through stop() method.
+ */
