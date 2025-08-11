53e0816ad2d4b4a66d7f19b236922f3285d17d29
Sadabrata Konar <b0278780@airtel.com>
2025-08-11 22:45:38 +0530

    stream api basics

--- Stream.java
+++ Stream.java
@@ -10,12 +10,6 @@ public class Stream {
         // Stream<Integer> s2= s1.filter(n ->n%2==0);
         // Stream<Integer> s3= s2.map(n->n*2);
         // int result=s3.reduce(0,(c,e)->c+e);
-        //
-        // s2.forEach(n -> System.out.println(n));
-        // s3.forEach(n -> System.out.println(n));
-        //
-        // s1.forEach(n-> System.out.println(n));
-        // s1.forEach(n-> System.out.println(n));
 
         int result = nums.stream()
                 .filter(n -> n % 2 == 0)
